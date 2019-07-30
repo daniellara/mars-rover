135d85ab162a4bf58f4e5b6bae68d1b0cf5f7227
Daniel Lara <daniel.lara.diezma@gmail.com>
2019-07-30 23:29:16 +0200

    Wrapp together all the modules to complete the functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function (input, output) {
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
+const missionBuilder = require('./mission_builder');
+
+module.exports = function(input, output) {
+	const fileContent = fs.readFileSync(path.resolve(input), 'utf8');
+	const missionInfo = missionBuilder(fileContent);
+
+	fs.writeFileSync(path.resolve(output), '', 'utf8')
+
+	for (let robot of missionInfo.robots) {
+		robot.executeOrders();
+		fs.appendFileSync(path.resolve(output), `${robot.stringFinalPosition()}${os.EOL}`, 'utf8');
+	}
 }
